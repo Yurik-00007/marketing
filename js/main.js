6b442fc8a66a3d4d4f217812e6018f6c22aedde3
Timofeev Yury <yurik-007@mail.ru>
2024-07-23 03:47:52 +0300

    Fancybox

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(function () {
-  var mixer = mixitup('.portfolio__content');
-  Fancybox.bind("[data-fancybox]", {});
+  var mixer = mixitup('.portfolio__content')
+  Fancybox.bind("[data-fancybox]", {})
+  $('.slider-block__inner').slick(
+    {
+      dots: true,
+      prevArrow:'<button type="button" class="slick-prev"><img src="../images/arrow-left.svg" alt="prev"></button>',
+      nextArrow:'<button type="button" class="slick-next"><img src="../images/arrow-right.svg" alt="next"></button>',
+    }
+  )
 })
