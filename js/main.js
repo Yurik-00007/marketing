7aaa22c1384db5f889c2c5a720a44646cfe89a5f
Timofeev Yury <yurik-007@mail.ru>
2024-07-29 03:23:59 +0300

    make adaptive to slider and team

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ $(function () {
       dots: true,
       prevArrow:'<button type="button" class="slick-prev"><img src="../images/arrow-left.svg" alt="prev"></button>',
       nextArrow:'<button type="button" class="slick-next"><img src="../images/arrow-right.svg" alt="next"></button>',
+      responsive: [
+        {
+          breakpoint: 769,
+          settings: {
+          arrows: false,
+          }
+        },
+      ]
     }
   )
   $('.menu__btn').on('click', function (e) {
